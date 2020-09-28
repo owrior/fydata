51554f04f6c9dd3910514f3084990dfda963346f
owrior <o.w.riordan@gmail.com>
2020-09-29 00:21:07 +0100

    Added script for fetching data. Created function for yahoo url creation.

---
+++ fydata/fetch_historical.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import datetime as dt
+
+
+def yahoo_url(ticker, start_date=dt.date(2010, 1, 1), end_date=dt.date.today(), period=None, frequency="1d"):
+    """
+
+    :param ticker:
+    :param start_date:
+    :param end_date:
+    :param period:
+    :param frequency: 1d, 1wk, 1mo
+    :return:
+    """
+    date_ref = dt.date(1970, 1, 1)
+
+    if period is None:
+        period1 = int((start_date - date_ref).total_seconds())
+    else:
+        start_date = end_date - period
+        period1 = int((start_date - date_ref).total_seconds())
+
+    period2 = int((end_date - date_ref).total_seconds())
+    root = "https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={frequency}&events=history"
+
+    built_url = root.format(ticker, period1, period2, frequency)

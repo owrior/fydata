b63d5d17611e1c3ce83fb6181498939dc81c3525
owrior <o.w.riordan@gmail.com>
2020-10-18 09:28:36 +0100

    Added init_db function.

--- fydata/sql_api.py
+++ fydata/sql_api.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 
 class SqlApi:
     def __init__(self):
-        self.engine = create_engine("mysql://fydata:money@localhost")
-        self.engine.connect()
+        self.engine = create_engine("mysql://fydata:money@localhost/fypy")
+
+    def init_db(self):
+        conn = self.engine.connect()
+        
 
     

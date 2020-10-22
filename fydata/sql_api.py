813e0f379375f267af1221039ea098e0f2a5be08
owrior <o.w.riordan@gmail.com>
2020-10-22 14:59:29 +0100

    Added function to insert dataframes in to sql easily. Begun writing data control functions.

--- fydata/sql_api.py
+++ fydata/sql_api.py
@@ -1,10 +1,32 @@
-from sqlalchemy import create_engine, 
+from sqlalchemy import create_engine 
 from sqlalchemy import Table, Column, String, Integer, Date, Numeric, ForeignKey, MetaData
-from yahoo_data import dl_yahoo
+from fydata.yahoo_data import dl_yahoo
 
 class SqlApi:
     def __init__(self):
         self.engine = create_engine("mysql://fydata:money@localhost/fypy")
+
+    def df_to_sql(self, df, table, key=None):
+        if key is None:
+            sql_statement = "INSERT INTO\n\t" + table + " (" + \
+            ",".join(df.columns) + ")\nVALUES\n"
+        else:
+            sql_statement = "INSERT INTO\n\t" + table + " (" \
+            + key + "," + ",".join(df.columns) + ")\nVALUES\n"
+
+        last = df.index[-1]
+        for index, row in df.iterrows():
+            if index == last:
+                sql_statement += "\t(" + \
+                    ",".join([ "'"+str(item)+"'" for item in row]) \
+                    + ");\n"
+            else:
+                sql_statement += "\t(" + \
+                    ",".join([ "'"+str(item)+"'" for item in row]) \
+                    + "),\n"
+
+        return(sql_statement)
+
 
     def init_db(self):
         meta = MetaData()
@@ -31,11 +53,16 @@ class SqlApi:
 
         meta.create_all(self.engine)
 
-    def update_ticker(self, ticker):
-        try:
-            conn = self.engine.connect()
-        except:
-            "Connection failed"
+    def new_ticker(self, ticker):
+        res = self.engine.execute("SELECT COUNT(*) AS num FROM tickers WHERE ticker = '" + ticker + "';").fetchall()
+        if res[0][0] > 0:
+            print("Ticker already in database.")
+            return 0
+
+        historic = dl_yahoo(ticker)        
+        historic.to_csv("~/Data/historic.csv")
+        
+        self.engine.execute("INSERT INTO tickers ('ticker') VALUES ('" + ticker + "');")
 
 
     
